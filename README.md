e368d758fc5d3ccc3b5e59795087dd4024207818
Mobarak.Alseif@tuwaiq.edu.sa <Mobarak.Alseif@tuwaiq.edu.sa>
2021-11-11 13:09:12 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# ServiceData
+
+### Package Description :
+ServiceData is an HTTP networking library written in Swift which can download different types of data.
+
+### Features List :
+- can download different types of items json, data..
+
+#### Usage :
+To use `ServiceData`, For example:
+
+```swift
+
+import ServiceData
+
+struct UserVM {
+    
+    func fetchData(completion: ([Users]) -> ()) {
+        ServiceData.fetchGenericData(from: "url", completion: completion)
+    }
+    
+}
+
+```
+## Author:
+
+Mubarak Alseif 👨‍💻
+info@easycode.co
---
+++ Sources/ServiceData/ServiceData.swift
@@ -0,0 +1,24 @@
+//
+//  ServiceData.swift
+//  ElmAssignment
+//
+//  Created by Mobark Alseif on 05/04/1443 AH.
+//
+
+import Foundation
+
+public class ServiceData: NSObject {
+    
+    // Generic function for fetch generic Data, with decode response.
+    
+    public static func fetchGenericData<T: Decodable>(from path: String, completion: ([T]) -> ()) {
+        
+        guard let url = URL(string: path), let data = try? Data(contentsOf: url) else { return }
+        let json = try? JSONDecoder().decode([T].self, from: data)
+        guard let result = json else { return }
+        
+        completion(result)
+        
+    }
+    
+}
